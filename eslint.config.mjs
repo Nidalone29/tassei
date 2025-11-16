04462607fe002ea95cf0d8dbf50cdf903ee6c34b
Nidal Guerouaja <22301275+Nidalone29@users.noreply.github.com>
2025-11-16 23:49:19 +0100

    Add SortInclude

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import nx from "@nx/eslint-plugin";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 export default [
   ...nx.configs["flat/base"],
@@ -9,6 +10,9 @@ export default [
   },
   {
     files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
     rules: {
       "@nx/enforce-module-boundaries": [
         "error",
@@ -23,6 +27,29 @@ export default [
           ],
         },
       ],
+      "simple-import-sort/imports": [
+        "warn",
+        {
+          groups: [
+            // Packages `react` related packages come first.
+            ["^react", "^@?\\w"],
+            // Side effect imports.
+            ["^\\u0000"],
+            // shadcn/ui components imports.
+            ["^@/components/ui"],
+            // Packages.
+            // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
+            ["^@?\\w"],
+            // Absolute imports and other imports such as Vue-style `@/foo`.
+            // Anything not matched in another group.
+            ["^"],
+            // Relative imports.
+            // Anything that starts with a dot.
+            ["^\\."],
+          ],
+        },
+      ],
+      "simple-import-sort/exports": "warn",
     },
   },
   {
